ec614475d04ceb595e392e7cf1c626bc0123a888
JAYANTHI-0406 <ja2603930@gmail.com>
2026-08-05 13:03:42 +0530

    Add files via upload

---
+++ Operateroprecendece.py
@@ -0,0 +1,20 @@
+
+#() → Parentheses
+#** → Power
+#* / // %
+#+ -
+#Shift operators (<<, >>)
+#Bitwise operators (&, ^, |)
+#Comparison (<, >, ==, etc.)
+#Assignment (=, +=, etc.)
+#Identity (is)
+#Membership (in)
+#Logical (not → and → or)
+
+3/2*4 + 3 + (10/5)**3 - 2
+print(3/2*4 + 3 + (10/5)**3 - 2)
+#(10/5)=2
+#2**3=8
+#3/2=1.5
+#1.5*4=6
+#6.0+3+8-2=15.0
